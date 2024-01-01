8274977f394d0993c92596318f1ca127b06d3fe4
Jaranya Boonthong <faiijaranya0159@gmail.com>
2024-01-01 17:45:48 +0700

    first03 commit

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
     
-    line = None
+    V_NB_LINES = 7
+    V_LINES_SPACING = .1 # precentage in screen width
+    vertical_line = []
+    
     def __init__(self, **kwargs):
         super(MainWidget,self).__init__(**kwargs)
         #print("INIT W:" + str(self.width) + " H:" + str(self.height))
@@ -22,7 +25,7 @@ class MainWidget(Widget):
         #print("ON PARENY W:" + str(self.width) + " H:" +str(self.height))
         # self.perspective_point_x = self.width/2
         # self.perspective_point_y = self.height * 0.75
-        self.init_vertical_lines()
+        self.update_vertical_lines() # เพื่ออัปเดตคุณสมบัติของเส้นที่ถูกวาดบน canvas ของ widget
         
     def on_perspective_point_x(self, widget, value):
         #print("PX:" + str(value))
@@ -35,11 +38,19 @@ class MainWidget(Widget):
     def init_vertical_lines(self):
         with self.canvas:
             Color(1, 1, 1) #กำหนดสีของเส้นเป็นขาว
-            self.line = Line(points=[self.width/2, 0, self.width/2, 100]) #ลักษณะของเส้นที่ถูกสร้าง [(x1, y1), (x2, y2)]
-            
+            #self.line = Line(points=[100, 0, 100, 100]) #ลักษณะของเส้นที่ถูกสร้าง
+            for i in range(0, self.V_NB_LINES):
+                self.vertical_line.append(Line(points=[0, 0, 0, 0]))
+                
     def update_vertical_lines(self):
-        self.line.point = []
-            
+        central_line_x = int(self.width / 2)
+        #self.line.points = [center_x, 0, center_x, 100] #ตำแหน่งของจุดทั้งหมดที่ใช้ในการสร้างเส้น (Line) บน canvas
+        spacing = self.V_LINES_SPACING * self.width
+        offset = -int(self.V_NB_LINES/2)
+        for i in range(0, self.V_NB_LINES):
+                line_x = int(central_line_x + offset * spacing)
+                self.vertical_line[i].points = [line_x, 0, line_x, self.height]
+                offset += 1
     
 class CompsuApp(App):
     def build(self):
