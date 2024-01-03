f44c44252d8a93f9dc3513e91f6bb6b5e2d2176b
Jaranya Boonthong <faiijaranya0159@gmail.com>
2024-01-03 16:29:10 +0700

    Perspective transformation

--- main.py
+++ main.py
@@ -49,8 +49,23 @@ class MainWidget(Widget):
         offset = -int(self.V_NB_LINES/2)
         for i in range(0, self.V_NB_LINES):
                 line_x = int(central_line_x + offset * spacing)
-                self.vertical_line[i].points = [line_x, 0, line_x, self.height]
+                x1, y1 = self.transform(line_x, 0)
+                x2, y2 = self.transform(line_x, self.height)
+                self.vertical_line[i].points = [x1, y1, x2, y2]
                 offset += 1
+                
+    def transform(self, x, y):
+        #return self.transform_2D(x, y)
+        return self.transform_perspective(x, y)
+    
+    def transform_2D(self, x, y):
+        return x, y
+    
+    def transform_perspective(self, x, y):
+        tr_y = y * self.perspective_point_y / self.height
+        if tr_y > self.perspective_point_y:
+            tr_y = self.perspective_point_y
+        return int(x), int(tr_y)
     
 class CompsuApp(App):
     def build(self):
